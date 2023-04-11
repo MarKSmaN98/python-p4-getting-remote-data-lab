3b8718e176fb42450835ba91de71ea3205929f58
Mark Coats <alexco1234@gmail.com>
2023-04-11 09:51:16 -0500

    fin

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,13 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        body = json.loads(response.content)
+        return body
